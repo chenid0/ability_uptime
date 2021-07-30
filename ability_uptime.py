614e9af6e3c61f38e3d954ccf5d6328882172d69
Chen, Isaac <Isaac.Chen@Teradata.com>
2021-07-29 22:45:53 -0700

    added multiple chances to tigger per round

--- ability_uptime.py
+++ ability_uptime.py
@@ -6,35 +6,40 @@ seed(1)
 ability_active = False
 active_rounds = 0
 num_rounds = 100_000_000
-ability_duration = 0
-first_ability_chance = 0.8
-first_ability_duration = 3
+cur_ability_duration = 0
 
-second_ability_chance = 0.5
-second_ability_duration = 2
+first_ability_chance = 0.8  # probability of ability triggering
+first_ability_duration = 3  # number of rounds it is active once triggered
+first_ability_num_chances = 1  # number of times it can trigger in a round
+
+second_ability_chance = 0.75  # probability of ability triggering
+second_ability_duration = 2  # number of rounds it is active once triggered
+second_ability_num_chances = 1  # number of times it can trigger in a round
 for round in range(num_rounds):
     # print(f"round {round+1}")
-    ability_duration -= 1
-    if ability_duration <= 0:
+    cur_ability_duration -= 1
+    if cur_ability_duration <= 0:
         ability_active = False
     if ability_active:
         # print("ability already active")
         pass
     else:
         # print("ability not active")
-        first_ability_roll = random()  # generate random numbers between 0-1
-        # print(f"first ability roll {first_ability_roll}")
-        if first_ability_roll <= first_ability_chance:
-            ability_active = True
-            ability_duration = first_ability_duration
-            # print(f"activating for {ability_duration} rounds ")
-        if not ability_active:
-            second_ability_roll = random()
-            # print(f"second ability roll {second_ability_roll}")
-            if second_ability_roll <= second_ability_chance:
+        for attempt in range(first_ability_num_chances):
+            first_ability_roll = random()  # generate random numbers between 0-1
+            # print(f"first ability roll {first_ability_roll}")
+            if not ability_active and first_ability_roll <= first_ability_chance:
                 ability_active = True
-                ability_duration = second_ability_duration
+                cur_ability_duration = first_ability_duration
                 # print(f"activating for {ability_duration} rounds ")
+        if not ability_active:
+            for attempt in range(second_ability_num_chances):
+                second_ability_roll = random()
+                # print(f"second ability roll {second_ability_roll}")
+                if not ability_active and second_ability_roll <= second_ability_chance:
+                    ability_active = True
+                    cur_ability_duration = second_ability_duration
+                    # print(f"activating for {ability_duration} rounds ")
     # print(f"ability active t/f: {ability_active}")
     if ability_active:
         active_rounds += 1
