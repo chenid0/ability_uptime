4c0b683a4d282581be9d149693c1b55c9585e7cd
Chen, Isaac <Isaac.Chen@Teradata.com>
2021-08-16 13:25:45 -0700

    fixing start of round check

--- ability_uptime.py
+++ ability_uptime.py
@@ -4,13 +4,14 @@ from random import random, seed
 seed(1)
 
 ability_active = False
-active_rounds = 0
+active_end_rounds = 0
+active_start_rounds = 0
 num_rounds = 100_000_000
-# num_rounds = 1000
+# num_rounds = 10
 cur_ability_duration = 0
 
-first_ability_chance = 0.8  # probability of ability triggering
-first_ability_duration = 3  # number of rounds it is active once triggered
+first_ability_chance = 0.0  # probability of ability triggering
+first_ability_duration = 2  # number of rounds it is active once triggered
 first_ability_num_chances = [1]  # number of times it can trigger in a round
 first_ability_chances_start_index = 0
 
@@ -22,36 +23,42 @@ second_ability_chances_start_index = 2
 oblit_activate = 0
 oblit_not_activate = 0
 for round in range(num_rounds):
-    #print(f"round {round+1}")
+    print(f"\nround {round+1}")
     cur_ability_duration -= 1
     if cur_ability_duration <= 0:
         ability_active = False
-    if ability_active:
-        # print("ability already active")
-        pass
 
-    # print("ability not active")
+    if ability_active:
+        # print("ability active at start of round before officer activation")
+        pass
+    else:
+        # print("ability not active at start of round before officer activation")
+        pass
     # print(first_ability_chances_start_index)
     num_chances = first_ability_num_chances[first_ability_chances_start_index]
-    for _ in range(num_chances):
+    for chance in range(num_chances):
         first_ability_roll = random()  # generate random numbers between 0-1
         # print(f"first ability roll {first_ability_roll}")
         if not ability_active and first_ability_roll <= first_ability_chance:
             ability_active = True
             cur_ability_duration = first_ability_duration
-            # print(f"activating for {ability_duration} rounds ")
+            # print(f"activating for {first_ability_duration} rounds ")
+
+    if ability_active:
+        # print("ability active after start of round officer")
+        active_start_rounds += 1
 
     # second officer nero
     num_chances2 = second_ability_num_chances[second_ability_chances_start_index]
-    #print(num_chances2)
+    # print(f"number of chances to proc: {num_chances2}")
     first = True
-    for _ in range(num_chances2):
+    for chance in range(num_chances2):
         second_ability_roll = random()
         # print(f"second ability roll {second_ability_roll}")
         if not ability_active and second_ability_roll <= second_ability_chance:
             ability_active = True
             cur_ability_duration = second_ability_duration
-            # print(f"activating for {ability_duration} rounds ")
+            # print(f"activating for {second_ability_duration} rounds ")
         if ability_active and second_ability_chances_start_index == 0 and first:
             oblit_activate += 1
         elif num_chances2 == 3 and first:
@@ -69,10 +76,11 @@ for round in range(num_rounds):
     )
     # print(f"ability active t/f: {ability_active}")
     if ability_active:
-        active_rounds += 1
+        active_end_rounds += 1
     # print(f"total num rounds active: {active_rounds}")
     # print(f"number of remaining rounds active: {ability_duration-1}")
 
-print(active_rounds / num_rounds)
-print(oblit_activate / (oblit_activate + oblit_not_activate))
+print(f"ability uptime for start of round: {active_start_rounds / num_rounds}")
+print(f"ability uptime for end of round: {active_end_rounds / num_rounds}")
+print(f"obliterator uptime: {oblit_activate / (oblit_activate + oblit_not_activate)}")
 
